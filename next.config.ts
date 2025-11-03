3fa2f286e2a0265c2e7365a8d8597fb5222c8a96
tonon <salvio.tonon@gmail.com>
2025-11-03 11:40:00 -0300

    allowdevorigins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["https://*.http.cloud.morph.so"],
 };
 
 export default nextConfig;
